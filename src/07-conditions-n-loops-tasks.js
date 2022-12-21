64a980b2d2933727ad69bf7999ecd40a19d198b0
github-usernameEXisTAnZ <ozim_ssspam@mail.ru>
2022-12-21 10:45:29 +0300

    feat: implement 8 task from conds-n-loops

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -200,8 +200,8 @@ function findFirstSingleChar(str) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
-  throw new Error('Not implemented');
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  return (isStartIncluded ? '[' : '(') + [a, b].sort(() => b - a).join(', ') + (isEndIncluded ? ']' : ')');
 }
 
 
