b716fd1053afab715b2ee109388456edde999f88
github-usernameEXisTAnZ <ozim_ssspam@mail.ru>
2022-12-21 17:10:03 +0300

    feat: implement 4 task from functions

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -84,8 +84,9 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const func2 = func();
+  return () => func2;
 }
 
 
