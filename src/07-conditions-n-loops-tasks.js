4a2833b4405b0887d097b01a6217a1a8dcaeb949
github-usernameEXisTAnZ <ozim_ssspam@mail.ru>
2022-12-21 15:11:56 +0300

    feat: implement 11 task from conds-n-loops

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -259,8 +259,11 @@ function reverseInteger(num) {
  *   5436468789016589 => false
  *   4916123456789012 => false
  */
-function isCreditCardNumber(/* ccn */) {
-  throw new Error('Not implemented');
+function isCreditCardNumber(ccn) {
+  const res = ccn.toString().split('').reverse()
+    .map((el, i) => (i % 2 ? el * 2 : +el))
+    .reduce((acc, cur) => acc + (cur > 9 ? cur - 9 : cur));
+  return !(res % 10);
 }
 
 /**
